87da064d3f492d28f670a07bc88067db3aceb3af
Arsenij Zorin <zorinarsenij@mail.ru>
2019-03-20 16:09:25 +0300

    [ADD] Schema for database

---
+++ migrations/0_initial.sql
@@ -0,0 +1,69 @@
+CREATE EXTENSION IF NOT EXISTS CITEXT;
+
+DROP TABLE IF EXISTS forum, thread, client, post, vote;
+
+CREATE TABLE client (
+                      id SERIAL PRIMARY KEY,
+                      email CITEXT NOT NULL UNIQUE,
+                      nickname CITEXT NOT NULL UNIQUE,
+                      fullname TEXT NOT NULL,
+                      about TEXT NOT NULL DEFAULT ''
+);
+
+CREATE TABLE forum (
+  id SERIAL PRIMARY KEY,
+
+  slug CITEXT NOT NULL UNIQUE,
+  title TEXT NOT NULL,
+
+  threads INTEGER NOT NULL DEFAULT 0,
+  posts BIGINT NOT NULL DEFAULT 0,
+
+  user_id INTEGER NOT NULL REFERENCES client(id),
+  user_nickname CITEXT NOT NULL
+);
+
+CREATE TABLE thread (
+  id SERIAL PRIMARY KEY,
+
+  slug CITEXT DEFAULT NULL,
+  title TEXT NOT NULL,
+  message TEXT NOT NULL,
+
+  forum_id INTEGER NOT NULL REFERENCES forum(id),
+  forum_slug CITEXT NOT NULL,
+
+  user_id INTEGER NOT NULL REFERENCES client(id),
+  user_nickname CITEXT NOT NULL,
+
+  created TIMESTAMPTZ,
+  votes INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE post (
+  id SERIAL PRIMARY KEY,
+
+  message TEXT NOT NULL,
+  created TIMESTAMPTZ,
+  is_edited BOOLEAN NOT NULL DEFAULT FALSE,
+
+  user_id INTEGER NOT NULL REFERENCES client(id),
+  user_nickname CITEXT NOT NULL,
+
+  thread_id INTEGER NOT NULL REFERENCES thread(id),
+
+  forum_slug CITEXT NOT NULL REFERENCES forum(slug),
+
+  parent INTEGER DEFAULT 0,
+  parents INT [] NOT NULL,
+  root INT NOT NULL
+);
+
+CREATE TABLE vote (
+  id SERIAL PRIMARY KEY,
+
+  voice BOOLEAN,
+
+  user_id INTEGER NOT NULL REFERENCES client(id),
+  thread_id INTEGER NOT NULL REFERENCES thread(id)
+);
