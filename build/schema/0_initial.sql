79d59a2eb984510390abea0012222786df1efd8e
Arsenij Zorin <zorinarsenij@mail.ru>
2019-06-10 13:44:00 +0300

    [ADD] Indexes

--- build/schema/0_initial.sql
+++ build/schema/0_initial.sql
@@ -10,6 +10,15 @@ CREATE TABLE client (
   about TEXT NOT NULL DEFAULT ''
 );
 
+CREATE INDEX client_covering_index
+  ON client(nickname, email, fullname, about);
+
+CREATE INDEX client_email_index
+  ON client(email);
+
+CREATE INDEX client_nickname_index
+  ON client(nickname);
+
 CREATE TABLE forum (
   id SERIAL PRIMARY KEY,
   slug CITEXT NOT NULL UNIQUE,
@@ -19,6 +28,12 @@ CREATE TABLE forum (
   user_id INTEGER NOT NULL REFERENCES client(id),
   user_nickname CITEXT NOT NULL
 );
+
+CREATE INDEX forum_slug_index
+  ON forum(slug);
+
+CREATE INDEX forum_covering_index
+  ON forum(slug, title, posts, threads, user_nickname);
 
 CREATE TABLE thread (
   id SERIAL PRIMARY KEY,
@@ -33,6 +48,22 @@ CREATE TABLE thread (
   votes INTEGER NOT NULL DEFAULT 0
 );
 
+CREATE INDEX thread_slug_index
+  ON thread(slug);
+
+CREATE INDEX thread_func_id_index
+  ON thread(text(id));
+
+CREATE INDEX thread_created_index
+  ON thread(forum_slug, created);
+
+CREATE INDEX thread_created_desc_index
+  ON thread(forum_slug, created DESC);
+
+-- ???
+CREATE INDEX thread_covering_index
+  ON thread(id, slug, title, message, forum_slug, user_nickname, created, votes);
+
 CREATE TABLE post (
   id SERIAL PRIMARY KEY,
   message TEXT NOT NULL,
@@ -46,6 +77,34 @@ CREATE TABLE post (
   parents INT [] NOT NULL,
   root INT NOT NULL
 );
+
+CREATE INDEX post_id_thread_index
+  ON post(id, thread_id);
+
+CREATE INDEX post_id_covering_index
+  ON post(id, message, created, is_edited, user_nickname, thread_id, forum_slug, parent);
+
+CREATE INDEX post_thread_covering_index
+  ON post(thread_id, id, message, created, is_edited, user_nickname, forum_slug, parent);
+
+CREATE INDEX post_tree_index
+  ON post(thread_id, array_append(parents, id));
+
+CREATE INDEX post_tree_desc_index
+  ON post(thread_id, array_append(parents, id) DESC);
+
+CREATE INDEX post_parent_tree_index
+  ON post(thread_id, id) WHERE parent = 0;
+
+CREATE INDEX post_parent_tree_desc_index
+  ON post(thread_id, id DESC) WHERE parent = 0;
+
+CREATE INDEX post_thread_index
+  ON post(thread_id, id);
+
+CREATE INDEX post_thread_desc_index
+  ON post(thread_id, id DESC);
+
 
 CREATE TABLE vote (
   id SERIAL PRIMARY KEY,
