ab45e7ffb9a825be4ccd82cda947b19e7d39d10a
Arsenij Zorin <zorinarsenij@mail.ru>
2019-06-16 11:57:24 +0300

    [UPD] Remove logger middleware

--- internal/app/delivery/http/api.go
+++ internal/app/delivery/http/api.go
@@ -7,7 +7,6 @@ import (
 	"github.com/ZorinArsenij/tech-db-forum/internal/app/delivery/http/handlers/thread"
 	"github.com/ZorinArsenij/tech-db-forum/internal/app/delivery/http/handlers/user"
 	"github.com/ZorinArsenij/tech-db-forum/internal/app/delivery/http/handlers/vote"
-	"github.com/ZorinArsenij/tech-db-forum/internal/app/delivery/http/middleware"
 	"github.com/ZorinArsenij/tech-db-forum/internal/app/usecase"
 	"github.com/buaazp/fasthttprouter"
 )
@@ -27,33 +26,33 @@ func NewRestApi(
 	router := fasthttprouter.New()
 
 	//User routes
-	router.POST("/api/user/:nickname/create", middleware.LoggerMiddleware(user.CreateUser(userInteractor)))
-	router.GET("/api/user/:nickname/profile", middleware.LoggerMiddleware(user.GetUserByNickname(userInteractor)))
-	router.POST("/api/user/:nickname/profile", middleware.LoggerMiddleware(user.UpdateUser(userInteractor)))
+	router.POST("/api/user/:nickname/create", user.CreateUser(userInteractor))
+	router.GET("/api/user/:nickname/profile", user.GetUserByNickname(userInteractor))
+	router.POST("/api/user/:nickname/profile", user.UpdateUser(userInteractor))
 
 	//Forum routes
-	router.POST("/api/forum/:slug", middleware.LoggerMiddleware(forum.CreateForum(forumInteractor)))
-	router.GET("/api/forum/:slug/details", middleware.LoggerMiddleware(forum.GetForum(forumInteractor)))
-	router.GET("/api/forum/:slug/users", middleware.LoggerMiddleware(forum.GetForumUsers(forumInteractor)))
+	router.POST("/api/forum/:slug", forum.CreateForum(forumInteractor))
+	router.GET("/api/forum/:slug/details", forum.GetForum(forumInteractor))
+	router.GET("/api/forum/:slug/users", forum.GetForumUsers(forumInteractor))
 
 	//Thread routes
-	router.GET("/api/thread/:slug_or_id/details", middleware.LoggerMiddleware(thread.GetThread(threadInteractor)))
-	router.GET("/api/forum/:slug/threads", middleware.LoggerMiddleware(thread.GetThreads(threadInteractor)))
-	router.POST("/api/forum/:slug/create", middleware.LoggerMiddleware(thread.CreateThread(threadInteractor)))
-	router.POST("/api/thread/:slug_or_id/details", middleware.LoggerMiddleware(thread.UpdateThread(threadInteractor)))
+	router.GET("/api/thread/:slug_or_id/details", thread.GetThread(threadInteractor))
+	router.GET("/api/forum/:slug/threads", thread.GetThreads(threadInteractor))
+	router.POST("/api/forum/:slug/create", thread.CreateThread(threadInteractor))
+	router.POST("/api/thread/:slug_or_id/details", thread.UpdateThread(threadInteractor))
 
 	//Post routes
-	router.GET("/api/post/:id/details", middleware.LoggerMiddleware(post.GetPost(postInteractor)))
-	router.POST("/api/post/:id/details", middleware.LoggerMiddleware(post.UpdatePost(postInteractor)))
-	router.POST("/api/thread/:slug_or_id/create", middleware.LoggerMiddleware(post.CreatePosts(postInteractor)))
-	router.GET("/api/thread/:slug_or_id/posts", middleware.LoggerMiddleware(post.GetPosts(postInteractor)))
+	router.GET("/api/post/:id/details", post.GetPost(postInteractor))
+	router.POST("/api/post/:id/details", post.UpdatePost(postInteractor))
+	router.POST("/api/thread/:slug_or_id/create", post.CreatePosts(postInteractor))
+	router.GET("/api/thread/:slug_or_id/posts", post.GetPosts(postInteractor))
 
 	//Vote routes
-	router.POST("/api/thread/:slug_or_id/vote", middleware.LoggerMiddleware(vote.CreateVote(voteInteractor)))
+	router.POST("/api/thread/:slug_or_id/vote", vote.CreateVote(voteInteractor))
 
 	//Service routes
-	router.GET("/api/service/status", middleware.LoggerMiddleware(service.GetStatus(serviceInteractor)))
-	router.POST("/api/service/clear", middleware.LoggerMiddleware(service.Clear(serviceInteractor)))
+	router.GET("/api/service/status", service.GetStatus(serviceInteractor))
+	router.POST("/api/service/clear", service.Clear(serviceInteractor))
 
 	return &Api{
 		Router: router,
