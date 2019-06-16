daa898cdea3ba28f21bc346ef5a2e81ae454b293
Arsenij Zorin <zorinarsenij@mail.ru>
2019-06-16 14:05:41 +0300

    [ADD] Forum clustering

--- build/schema/1_cluster.sql
+++ build/schema/1_cluster.sql
@@ -2,3 +2,4 @@ CLUSTER client USING client_covering_index;
 CLUSTER thread USING thread_created_index;
 CLUSTER post USING post_thread_id_index;
 CLUSTER forum_client USING forum_client_covering_index;
+CLUSTER forum USING forum_slug_index;
